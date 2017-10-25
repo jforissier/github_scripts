60a02e756277988fc3a9934948229c6dc9067e13
Jerome Forissier <jerome.forissier@linaro.org>
2017-10-25 18:17:26 +0000

    Add hikey960
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>

--- my_optee_admin_make_stable.sh
+++ my_optee_admin_make_stable.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-export MY_ALLREPO='default qemu_v8 fvp hikey hikey_debian juno mt8173-evb rpi3 dra7xx am43xx am57xx'
+export MY_ALLREPO='default qemu_v8 fvp hikey hikey_debian hikey960 juno mt8173-evb rpi3 dra7xx am43xx am57xx'
 
 if [ -e "$MY_OPTEE_RELEASE_ROOT" ] ; then
 	echo "Remove the used directory first using"
